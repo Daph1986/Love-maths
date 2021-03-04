d438ed22a97034255005706ccaf41c97792c4001
Daph1986 <daphne.heimgartner@gmail.com>
2021-03-04 11:41:47 +0100

    Added  division parts

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unkown game type ${gameType}`);
         throw `Unkown game type ${gameType}, aborting!`;
@@ -91,6 +93,8 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "/") {
+        return [parseInt(operand1 / operand2), "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, aborting!`;
@@ -135,4 +139,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = (operand1 * operand2);
+    document.getElementById("operand2").textContent =  operand2;
+    document.getElementById("operator").textContent =  "/";
 }
